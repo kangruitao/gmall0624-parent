8b9c88200dd457dc4b21d12017f876427b22e254
kang <483239965@qq.com>
2019-11-26 08:44:31 +0800

    Initial commit

---
+++ gmall0624-logger/src/main/java/com/atguigu/gmall0624/logger/controller/LoggerController.java
@@ -0,0 +1,45 @@
+package com.atguigu.gmall0624.logger.controller;
+
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.atguigu.gmall0624.common.constant.GmallConstant;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@Slf4j
+public class LoggerController {
+
+    @Autowired
+    KafkaTemplate<String,String> kafkaTemplate;
+
+    @PostMapping("log")
+    public String log(@RequestParam("logString") String logString) {
+
+        System.out.println(logString);
+
+        //1、补时间戳
+        JSONObject jsonObject = JSON.parseObject(logString);
+        jsonObject.put("ts", System.currentTimeMillis());
+
+        //2、落盘日志文件
+        String jsonString = jsonObject.toJSONString();
+        log.info(jsonString);
+
+        //3、发送kafka
+        //3  发送kafka
+        if(jsonObject.getString("type").equals("startup")){
+            kafkaTemplate.send(GmallConstant.KAFKA_TOPIC_STARTUP,jsonString);
+        }else{
+            kafkaTemplate.send(GmallConstant.KAFKA_TOPIC_EVENT,jsonString);
+        }
+        return "ok";
+
+
+    }
+}
